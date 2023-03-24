6f32d141ae1a37dadbc05ece63b45adb221ecec9
Pavan Kumar Kandapagari <pavan.kandapagari@agile-robots.com>
2023-03-24 19:00:04 +0100

    feat: added testing platform

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -1,5 +1,5 @@
-#![no_std] // don't link the Rust standard library
-#![no_main] // disable all Rust-level entry points
+#![no_std]
+#![no_main]
 #![feature(custom_test_frameworks)]
 #![test_runner(runix::test_runner)]
 #![reexport_test_harness_main = "test_main"]
@@ -7,23 +7,19 @@
 use core::panic::PanicInfo;
 use runix::println;
 
-#[no_mangle]
+#[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-    #[cfg(test)]
     test_main();
+
     loop {}
 }
 
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    println!("{}", _info);
-    loop {}
-}
-
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     runix::test_panic_handler(info)
+}
+
+#[test_case]
+fn test_println() {
+    println!("test_println output");
 }
